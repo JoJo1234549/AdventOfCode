b6af4113d093ebb8bc358e26779d73b5b7301643
JoJo1234549 <wwfunbrickco@gmail.com>
2021-12-24 22:14:54 +0000

    completed day 6 part 2

--- 2021/day6/solution.py
+++ 2021/day6/solution.py
@@ -24,6 +24,19 @@ def calc_days_dict(fishies, days):
     fish_dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
     for fish in fishies:
         fish_dict[fish] += 1
+    
+    for i in range(days):
+        fish_dict = {8: fish_dict[0],
+                     7: fish_dict[8],
+                     6: fish_dict[7] + fish_dict[0],
+                     5: fish_dict[6],
+                     4: fish_dict[5],
+                     3: fish_dict[4],
+                     2: fish_dict[3],
+                     1: fish_dict[2],
+                     0: fish_dict[1]
+                     }
+    return fish_dict
 
 def part1_solution(fishies):
     for i in range(80):
@@ -32,6 +45,10 @@ def part1_solution(fishies):
     return len(fishies)
 
 def part2_solution(fishies):
-    pass
+    fishies = calc_days_dict(fishies, 256)
+    
+    return sum(fishies.values())
 
 print(part1_solution(format_data(read_file("input.txt"))))
+
+print(part2_solution(format_data(read_file("input.txt"))))
