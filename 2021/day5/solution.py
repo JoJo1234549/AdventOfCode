c967136a1a409e35502fc0562420adf92620dc10
JoJo1234549 <wwfunbrickco@gmail.com>
2021-12-24 21:23:04 +0000

    completed day 5 part 2

--- 2021/day5/solution.py
+++ 2021/day5/solution.py
@@ -37,6 +37,37 @@ def plot_lines(lines):
                     
     return points
 
+def plot_diagonals(lines, points):
+    def add_point(point):
+        try:
+            points[f"{point[0]},{point[1]}"] += 1
+        except KeyError:
+            points[f"{point[0]},{point[1]}"] = 1
+            
+    for x1, y1, x2, y2 in lines:
+        try:
+            m = (y2 - y1)/(x2 - x1)
+        except ZeroDivisionError:
+            m = 0
+        if m == 1:
+            for i in range(0, max(x1, x2) - min(x1, x2) + 1):
+                add_point([min(x1, x2) + i, min(y1, y2) + i])
+        elif m == -1:
+            for i in range(0, max(x1, x2) - min(x1, x2) + 1):
+                add_point([min(x1, x2) + i, max(y1, y2) - i])
+    return points
+
+def draw_diagram(points):
+    diagram = ""
+    for y in range(0, 10):
+        for x in range(0, 10):
+            try:
+                diagram += str(points[f"{x},{y}"])
+            except KeyError:
+                diagram += "."
+        diagram += "\n"
+    return diagram[:-1]
+
 def part1_solution(points):
     total = 0
     for x in points.values():
@@ -44,4 +75,17 @@ def part1_solution(points):
         
     return total
 
+def part2_solution(points):
+    total = 0
+    for x in points.values():
+        total += 1 if x >= 2 else 0
+        
+    return total
+
 print(part1_solution(plot_lines(format_data(read_file("input.txt")))))
+
+data = format_data(read_file("input.txt"))
+points = plot_lines(data)
+points = plot_diagonals(data, points)
+
+print(part2_solution(points))
--- 2021/day5/test_solutions.py
+++ 2021/day5/test_solutions.py
@@ -1,4 +1,4 @@
-from .solution import plot_lines, read_file, format_data, part1_solution
+from .solution import plot_lines, read_file, format_data, plot_diagonals, draw_diagram, part1_solution, part2_solution
 
     # Arrange
     
@@ -44,6 +44,43 @@ def test_plot_lines():
     # Assert
     
     assert result["7,4"] == 2
+    
+def test_plot_diagonals():
+    # Arrange
+    
+    data = format_data(read_file("sample_input.txt"))
+    points = plot_lines(data)
+    
+    # Act
+    
+    result = plot_diagonals(data, points)
+    
+    # Assert
+    
+    assert result["4,4"] == 3
+    
+def test_diagram():
+    # Arrange
+    
+    data = format_data(read_file("sample_input.txt"))
+    points = plot_diagonals(data, plot_lines(data))
+    
+    # Act
+    
+    result = draw_diagram(points)
+    
+    # Assert
+    
+    assert result == """1.1....11.
+.111...2..
+..2.1.111.
+...1.2.2..
+.112313211
+...1.2....
+..1...1...
+.1.....1..
+1.......1.
+222111...."""
 
 def test_part1_solution():
     # Arrange
@@ -58,3 +95,18 @@ def test_part1_solution():
     # Assert
     
     assert result == 5
+
+def test_part2_solution():
+    # Arrange
+    
+    data = format_data(read_file("sample_input.txt"))
+    points = plot_lines(data)
+    points = plot_diagonals(data, points)
+    
+    # Act
+    
+    result = part2_solution(points)
+    
+    # Assert
+    
+    assert result == 12
