44fe093d2949e1f406cedcdfb1eb286e4d18d5da
JoJo1234549 <wwfunbrickco@gmail.com>
2021-12-24 15:45:39 +0000

    day4-p1
    Day 4 Part 1 completed

--- 2021/day4/solution.py
+++ 2021/day4/solution.py
@@ -1,5 +1,68 @@
-with open("input.txt") as f:
-    _input = f.read().split("\n")
+import os
 
-## Part 1 ##
+dir = os.path.dirname(__file__)
 
+def read_file(filename):
+    file = open(dir + "/" + filename, "r")
+    return file.read()
+
+def format_data(data):
+    parts = data.split("\n\n")
+    bingo_numbers = [int(value) for value in parts[0].split(",")]
+
+    bingo_grids = parts[1:]
+
+    bingo_grids = [grid.split("\n") for grid in bingo_grids]
+    bingo_grids = [[[int(value) for value in line.split()] for line in grid] for grid in bingo_grids]
+    
+    return [bingo_numbers, bingo_grids]
+
+def is_win(numbers, board):
+    for row in board:
+        complete = True
+        for num in row:
+            if num not in numbers:
+                complete = False
+                break
+        if complete == True:
+            return True
+    
+    for column in range(5):
+        complete = True
+        for row in board:
+            if row[column] not in numbers:
+                complete = False
+                break
+        if complete == True:
+            return True
+    
+    return False
+
+def bingo_sim(numbers, board):
+    for index, number in enumerate(numbers):
+        status = is_win(numbers[:index + 1], board)
+        if status == True:
+            return [board, index]
+    return [board, None]
+
+def part1_solution(_input):
+    numbers, boards = format_data(_input)
+    quickest = [None, None]
+    for board in boards:
+        sim_board, win_index = bingo_sim(numbers, board)
+        if not quickest[1]:
+            quickest = [board, win_index]
+        if win_index and win_index < quickest[1]:
+            quickest = [board, win_index]
+    
+    board_numbers = sum(quickest[0], [])
+    for number in numbers[:quickest[1] + 1]:
+        try:
+            board_numbers.remove(number)
+        except ValueError:
+            pass
+    
+    score = sum(board_numbers) * numbers[quickest[1]]
+    return score
+
+print(solution(read_file("input.txt")))
---
+++ 2021/day4/test_solutions.py
@@ -0,0 +1,75 @@
+from solution import read_file, format_data, is_win, bingo_sim, solution
+
+    # Arrange
+    
+    # Act
+    
+    # Assert
+
+def test_readfile():
+    # Arrange
+
+    filename = "sample_input.txt"
+
+    # Act
+
+    result = len(read_file(filename).splitlines())
+
+    # Assert
+
+    assert result == 19
+
+def test_format_data():
+    # Arrange
+
+    _input = read_file("sample_input.txt")
+
+    # Act
+
+    result = format_data(_input)
+
+    # Assert
+    
+    assert result[0] == [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
+    assert result[1][0] == [[22,13,17,11,0], [8,2,23,4,24], [21,9,14,16,7], [6,10,3,18,5], [1,12,20,15,19]]
+
+def test_is_win():
+    # Arrange
+    
+    board = [[14,21,17,24,4], [10,16,15,9,19], [18,8,23,26,20], [22,11,13,6,5], [2,0,12,3,7]]
+    
+    # Act
+    
+    # Assert
+    
+    assert is_win([7,4,9,5,11,17,23,2,0,14,21,24], board) == True
+    assert is_win([7,4,9,5,11,17,23,2,0,14], board) == False
+    assert is_win([7,4,9,5,11,18,23,2,0,14,10,22], board) == True
+    assert is_win([7,4,9,5,11,18,23,2,0,14], board) == False
+
+def test_bingo_sim():
+    # Arrange
+    
+    _input = read_file("sample_input.txt")
+    data = format_data(_input)
+    
+    # Act
+    
+    # Assert
+    
+    assert bingo_sim(data[0], data[1][2]) == [[[14,21,17,24,4], [10,16,15,9,19], [18,8,23,26,20], [22,11,13,6,5], [2,0,12,3,7]], 11]
+    assert bingo_sim(data[0], data[1][0]) == [[[22,13,17,11,0], [8,2,23,4,24], [21,9,14,16,7], [6,10,3,18,5], [1,12,20,15,19]], 13]
+
+def test_part1_solution():
+    # Arrange
+
+    filename = "sample_input.txt"
+    _input = read_file(filename)
+
+    # Act
+
+    result = solution(_input)
+    
+    # Assert
+
+    assert result == 4512
