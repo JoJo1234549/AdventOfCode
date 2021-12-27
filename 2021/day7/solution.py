68835095c647017cf3e449fc500076bc67e7b86c
= <=>
2021-12-27 15:46:43 +0000

    completed day 7 part 1 + 2

--- 2021/day7/solution.py
+++ 2021/day7/solution.py
@@ -1,7 +1,59 @@
-import os
+import os, math
 
 dir = os.path.dirname(__file__)
 
 def read_file(filename):
     file = open(dir + "/" + filename, "r")
     return file.read()
+
+def format_data(data):
+    return [int(value) for value in data.split(",")]
+
+def find_mean(positions):
+    return sum(positions)/len(positions)
+
+def find_st_dev(values):
+    mean = find_mean(values)
+    return ((sum([(value - mean)**2 for value in values]) / len(values))**0.5)
+
+def calc_fuel(positions, target):
+    total = 0
+    for position in positions:
+        total += abs(target - position)
+    return total
+
+def calc_compound_fuel(positions, target):
+    total = 0
+    for position in positions:
+        total += sum(range(0, abs(target - position) + 1))
+    return total
+
+def part1_solution(positions):
+    mean = find_mean(positions)
+    stdev = find_st_dev(positions)
+    minimum = None
+    for i in range(max(math.floor(mean-stdev), 0), math.ceil(mean+stdev) + 1):
+        fuel = calc_fuel(positions, i)
+        if not minimum:
+            minimum = fuel
+        elif fuel < minimum:
+            minimum = fuel
+    
+    return minimum
+
+def part2_solution(positions):
+    mean = find_mean(positions)
+    stdev = find_st_dev(positions)
+    minimum = None
+    for i in range(max(math.floor(mean-stdev), 0), math.ceil(mean+stdev) + 1):
+        fuel = calc_compound_fuel(positions, i)
+        if not minimum:
+            minimum = fuel
+        elif fuel < minimum:
+            minimum = fuel
+    
+    return minimum
+
+print(part1_solution(format_data(read_file("input.txt"))))
+
+print(part2_solution(format_data(read_file("input.txt"))))
--- 2021/day7/test_solutions.py
+++ 2021/day7/test_solutions.py
@@ -1,4 +1,4 @@
-from .solution import read_file
+from .solution import part1_solution, part2_solution, read_file, format_data, find_mean, find_st_dev, calc_fuel, calc_compound_fuel
 
 def test_readfile():
     # Arrange
@@ -7,8 +7,99 @@ def test_readfile():
 
     # Act
 
-    result = len(read_file(filename))
+    result = len(read_file(filename).split(","))
 
     # Assert
 
-    assert result == 
+    assert result == 10
+    
+def test_format_data():
+    # Arrange
+    
+    data = read_file("sample_input.txt")
+    
+    # Act
+    
+    result = format_data(data)
+    
+    # Assert
+    
+    assert result == [16,1,2,0,4,2,7,1,2,14]
+    
+def test_find_mean():
+    # Arrange
+    
+    data = format_data(read_file("sample_input.txt"))
+    
+    # Act
+    
+    result = find_mean(data)
+    
+    # Assert
+    
+    assert result == 4.9
+    
+def test_find_st_dev():
+    # Arrange
+    
+    data = format_data(read_file("sample_input.txt"))
+    
+    # Act
+    
+    result = find_st_dev(data)
+    
+    # Assert
+    
+    assert result == 5.39351462406472
+    
+def test_calc_fuel():
+    # Arrange
+    
+    data = format_data(read_file("sample_input.txt"))
+    
+    # Act
+    
+    result = calc_fuel(data, 2)
+    
+    # Assert
+    
+    assert result == 37
+    
+def test_calc_compound_fuel():
+    # Arrange
+    
+    data = format_data(read_file("sample_input.txt"))
+    
+    # Act
+    
+    result = calc_compound_fuel(data, )
+    
+    # Assert
+    
+    assert result == 168
+    
+def test_part1_solution():
+    # Arrange
+    
+    data = format_data(read_file("sample_input.txt"))
+    
+    # Act
+    
+    result = part1_solution(data)
+    
+    # Assert
+    
+    assert result == 37
+    
+def test_part2_solution():
+    # Arrange
+    
+    data = format_data(read_file("sample_input.txt"))
+    
+    # Act
+    
+    result = part2_solution(data)
+    
+    # Assert
+    
+    assert result == 168
